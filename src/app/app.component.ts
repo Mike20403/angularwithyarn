870bde44064b1897035a399f95b7ab4eec344419
Mike20403 <maikhuong2003@gmail.com>
2023-09-11 16:08:54 +0700

    feat: Add environments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { environment } from '@app/env';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularyarn';
+
+  environment = environment;
 }
